b6c9651ff04d3f032480a3f3c5ffef2b2f6bff26
frantic-dev <okeyamazing@gmail.com>
2023-10-05 21:02:46 +0100

    5.7 blog list frontend finished

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,8 +1,33 @@
-const Blog = ({ blog }) => (
-  <div>
-    Title: <span style={{ color: 'blue' }}> {blog.title}</span> Author:{' '}
-    <span style={{ color: 'red' }}>{blog.author} </span>
-  </div>
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+  const buttonLabel = visible ? 'view' : 'hide'
+  const display = visible ? '' : 'none'
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  function toggleVisibility() {
+    setVisible(!visible)
+  }
+  return (
+    <div style={blogStyle}>
+      Title: <span style={{ color: 'blue' }}> {blog.title}</span>
+      <button onClick={toggleVisibility}>{buttonLabel}</button>
+      <div style={{ display: display }}>
+        url: {blog.url}
+        <br />
+        likes: {blog.likes}
+        <br />
+        Author: <span style={{ color: 'red' }}>{blog.author} </span>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
